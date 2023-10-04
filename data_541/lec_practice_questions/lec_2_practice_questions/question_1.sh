76016aec274301e6d1feefc2bb84677432525ee5
shethdhun1997@gmail.com <dhun97@mail.ubc.ca>
2023-10-04 13:00:14 -0700

    removing files

--- data_541/lec_practice_questions/lec_2_practice_questions/question_1.sh
+++
@@ -1,9 +0,0 @@
-read -p "Enter a number: " num1
-
-if (($num1 > 0)); then 
-	echo "Greater than 0"
-elif (($num1 < 0)); then
-	echo "Less than 0"
-else
-	echo "Number is 0"
-fi
--- data_541/lec_practice_questions/lec_2_practice_questions/question_2.sh
+++
@@ -1,6 +0,0 @@
-read -p 'Enter 2 numbers: ' num1 num2
-echo "sum is $((num1+num2))"
-echo "difference is $((num1-num2))"
-echo "product is $((num1*num2))"
-echo -n "quoties is" 
-echo "scale=2; $num1 / $num2" | bc
